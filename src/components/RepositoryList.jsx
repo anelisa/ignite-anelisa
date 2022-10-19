1a0ac3f7d8a19ee69ba71242d58b31f41ba732e3
anelisa <anelisa.silva25@gmail.com>
2022-10-18 22:04:50 -0300

    feat: add state

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
 import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
+
+//https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: "unform",
@@ -7,15 +11,24 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]); //sempre que for uma lista eu começo o estado com um array vazio
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem repository={repository} key={repository.name} />
+          );
+        })}
       </ul>
     </section>
   );
